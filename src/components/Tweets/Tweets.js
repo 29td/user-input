ce445f152abc3e65886bb56e2682c4553c645530
29td <nonhlanhlamndebele9@gmail.com>
2022-11-27 13:03:48 +0200

    Added tweets

--- src/components/Tweets/Tweets.js
+++ src/components/Tweets/Tweets.js
@@ -1,12 +1,40 @@
 import React from "react";
+require('dotenv').config();
+const {TwitterClient} = require('twitter-api-client');
+const axios = require('axios')
 
 const Tweet = () => {
   
-  return (
-    <>
-    <h6>Latest Tweets</h6>
-    </>
-  );
+  const twitterClient = new TwitterClient({
+
+      apiKey: process.env.TWITTER_API_KEY,
+      apiSecret: process.env.TWITTER_API_SECRET,
+      accessToken: process.env.TWITTER_ACCESS_TOKEN,
+      accessTokenSecret: process.env.TWITTER_BEARER_TOKEN
+  })
+  
+  axios.get('http://history.muffinlabs.com/date')
+      .then(response => {
+      const data = response.data.data ? response.data.data : {}
+      let tweet
+      if (data.Events && data.Events.length) {
+          //tweet the first event in the array
+          tweet = 'Year ' + data.Events[0].year + ' - ' + data.Events[0].text
+      } else {
+          tweet = 'Nothing happened today :)'
+      }
+  
+      twitterClient.tweets.statusesUpdate({
+          status: tweet
+      }).then (response => {
+          console.log("Tweeted!", response)
+      }).catch(err => {
+          console.error(err)
+      })
+  }).catch (err => {
+      console.error(err)
+  })
+  
 };
 
 export default Tweet;
